b8fbdc273addf1ba2b41e0a83b525ad221e91839
Justin.Wang <wangrongchao@126.com>
2022-06-16 14:31:58 +0800

    移除平台和测试框架选择

--- setup/ConfigureiOS.rb
+++ setup/ConfigureiOS.rb
@@ -15,7 +15,7 @@ module Pod
 
       keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
 
-      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Specta", "Kiwi", "None"]).to_sym
+      framework = "None"
       case framework
         when :specta
           configurator.add_pod_to_podfile "Specta"
